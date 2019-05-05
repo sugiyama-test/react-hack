e5a1a946974e107cb7e1bf2468e927b1c8cca9a4
ko_ohara <koudai_ohara@respawn.jp>
2019-05-05 18:59:58 +0900

    font change

--- src/components/Anime.js
+++ src/components/Anime.js
@@ -8,31 +8,44 @@ import Typography from '@material-ui/core/Typography';
 const styles = {
   // Cards
   card: {
-    width:'80%',
-    borderRadius:20,
+    width: "80%",
+    borderRadius: 20,
     marginTop: 20,
-    marginBottom: 20,
+    marginBottom: 20
   },
   empty: {
     width: 330,
     height: 0,
     margin: 0,
-    padding: 0,
+    padding: 0
   },
   bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)"
   },
   title: {
-    fontSize: 20 ,
+    fontSize: 16
   },
-
-  title2: {
-    margin:10,
-    fontSize: 16 ,
+  release: {
+    margin: 10,
+    fontSize: 20, 
+    fontFamily: "cursive",
   },
-
+  description: {
+    color: "#088A29",
+    margin: 10,
+    fontSize: 16,
+    fontFamily: "cursive"
+  },
+  movieN: {
+    fontSize: 24,
+    fontFamily: "cursive",
+  },
+  staff: {
+    fontSize: 16,
+    fontFamily: "cursive",
+  }
 };
 
 function Anime(props) {
@@ -40,38 +53,49 @@ function Anime(props) {
 
     return (
       <Card className={classes.card}>
-          <CardContent>
-            {release_date !== '' &&
-              <Typography className={classes.title} color="textSecondary">
-                {release_date}
-              </Typography>
-            }
-            <Typography variant="headline" component="h2">
-              {title}
+        <CardContent>
+          {release_date !== "" && (
+            <Typography className={classes.release} color="textSecondary">
+              {release_date}
             </Typography>
-            {director !== '' &&
-              <Typography className={classes.title2} color="textSecondary">
-                {director}
-              </Typography>
-            }
-            {release_date !== '' &&
-              <Typography className={classes.title2} color="textSecondary">
-                {description}
-              </Typography>
-            }
-            {producer !== '' &&
-              <Typography className={classes.title2} color="textSecondary">
-                {producer}
-              </Typography>
-            }
-            {title !== '' &&
-              <Typography className={classes.title} color="textSecondary">
-                <a href={'https://twitter.com/search?q=%23'+ title} target="_blank" rel="noopener noreferrer">
-                  #{title}
-                </a>
-              </Typography>
-            }
-          </CardContent>
+          )}
+          <Typography
+            className={classes.movieN}
+            variant="headline"
+            component="h2"
+          >
+            {title}
+          </Typography>
+          {release_date !== "" && (
+            <Typography
+              className={classes.description}
+              color="textSecondary"
+            >
+              {description}
+            </Typography>
+          )}
+          {director !== "" && (
+            <Typography className={classes.staff} color="textSecondary">
+              Director：{director}
+            </Typography>
+          )}
+          {producer !== "" && (
+            <Typography className={classes.staff} color="textSecondary">
+              Producer：{producer}
+            </Typography>
+          )}
+          {title !== "" && (
+            <Typography className={classes.title} color="textSecondary">
+              <a
+                href={"https://twitter.com/search?q=%23" + title}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                #{title}
+              </a>
+            </Typography>
+          )}
+        </CardContent>
       </Card>
     );
 }
