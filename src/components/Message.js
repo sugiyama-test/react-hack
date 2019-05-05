9b67086f44fe17fa86044c4de5c90e3753da8a32
respawn_sugiyama <sugiyama@respawn.tokyo>
2019-05-05 17:32:16 +0900

    コメントの変更

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Avatar from 'material-ui/Avatar';
 import List from 'material-ui/List/List';
 import ListItem from 'material-ui/List/ListItem';
